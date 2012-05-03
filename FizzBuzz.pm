412f8448b0aacf1d9cc4cc7cef9b0da06041d4d3
Dew Saosoong <dew@abctech-thailand.com>
2012-05-03 18:07:25 +0700

    all test passed

--- FizzBuzz.pm
+++ FizzBuzz.pm
@@ -1,3 +1,31 @@
 package FizzBuzz;
 
+sub new {
+    my $class = shift;
+    my $self  = { };
+
+    bless $self, $class;
+    return $self;
+}
+
+sub fizzbuzz {
+    my ($self, $number) = @_;
+    
+    if (($number % 3 == 0) && ($number % 5 == 0)) {
+        return 'FizzBuzz';
+    }
+    elsif ($number % 3 == 0) {
+        return 'Fizz';
+    }
+    elsif ($number % 5 == 0) {
+        return 'Buzz';
+    }
+    else {
+        return $number;
+    }
+
+}
+
+my $test = new FizzBuzz();
+
 1;
